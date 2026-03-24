6aed683b04c7b2666f2419c32241f03f40d4e47c
unknown <alunoD14@acad.unifor.local>
2026-03-24 20:23:38 -0300

    Arquivo add

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        
+        double numero1 = 29.92;
+        //numero1 = "Lucas"; não pode mudar o tipo por ser uma linguagem estatica
+        int numero2 = (int) numero1;
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+
+    }
+}
